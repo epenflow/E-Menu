a6f4b14faf815cb3ad4eae64d8e5aa59a3ef33b8
epenflow <epenflow@gmail.com>
2025-03-22 20:42:54 +0800

    refactor

--- src/pages/private/role.tsx
+++ src/pages/private/role.tsx
@@ -1,17 +1,9 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { getRouteApi } from "@tanstack/react-router";
 import { PlusIcon } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import PrivateContainer from "~/layouts/private/private-container";
-import { allRoleQueryOptions } from "~/lib/services/role";
 import type { Role } from "~/lib/types";
 
 const Role = () => {
-  const { routeApi } = resources;
-  const {
-    data: { data: pagination },
-  } = useSuspenseQuery(allRoleQueryOptions(routeApi.useSearch()));
-
   return (
     <PrivateContainer>
       <div className="w-full h-auto flex gap-2 flex-col overflow-x-hidden">
@@ -26,11 +18,3 @@ const Role = () => {
   );
 };
 export default Role;
-const resources = {
-  routeApi: getRouteApi("/_private/role"),
-};
-
-export interface Artwork {
-  artist: string;
-  art: string;
-}
